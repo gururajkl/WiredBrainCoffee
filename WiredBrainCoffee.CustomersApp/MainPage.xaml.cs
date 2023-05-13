7a93007d2bae959e2af4eede2c819652cdd99f06
Gururaj KL <gururajragavendra7@gmail.com>
2023-05-13 21:22:03 +0530

    Add project files.

---
+++ WiredBrainCoffee.CustomersApp/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace WiredBrainCoffee.CustomersApp
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void AddCustomerButton_Click(object sender, RoutedEventArgs e)
+        {
+            var messageDialog = new MessageDialog("Customer added!");
+            await messageDialog.ShowAsync();
+        }
+    }
+}
